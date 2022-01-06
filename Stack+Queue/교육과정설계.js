0934e8c889184c422ad56454e0a4636fa370529e
kimin3004 <kimin3004@gmail.com>
2022-01-07 01:22:27 +0900

    refactor: 교육과정설계

--- Stack+Queue/교육과정설계.js
+++ Stack+Queue/교육과정설계.js
@@ -3,16 +3,19 @@ function solution(str) {
     .split('\n')
     .map((el) => el.split(''));
 
-  while (curriculum.length) {
-    curriculumRule[curriculumRule.length - 1] === curriculum.pop() &&
-      curriculumRule.pop();
+  for (let i = curriculum.length - 1; i >= 0; i--) {
+    if (curriculumRule.includes(curriculum[i])) {
+      if (curriculumRule.pop() !== curriculum[i]) {
+        return 'NO';
+      }
+    }
   }
-
   return !curriculumRule.length ? 'YES' : 'NO';
 }
 
 console.log(
   solution(`CBA
-CDAGEB`)
+  FCDGEBA`)
 );
 // YES
+// 순회 문제일 시 초반에 early return 할 수 있는 조건이 있는지 확인하기
