17c01567bfb0413a6ab451fad6fc4727f8660227
kimin3004 <kimin3004@gmail.com>
2022-01-12 21:44:12 +0900

    refactor: 버블정렬

--- Sort+Greedy/버블정렬.js
+++ Sort+Greedy/버블정렬.js
@@ -2,7 +2,7 @@ function solution(str) {
   const arr = str.split(' ').map((char) => Number(char));
 
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i; j < arr.length - i - 1; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
