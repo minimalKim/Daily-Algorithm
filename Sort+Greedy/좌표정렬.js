7d98d07623af17679681f275b37ddd8c80390fe8
kimin3004 <kimin3004@gmail.com>
2022-01-12 21:43:52 +0900

    feat: 좌표정렬

---
+++ Sort+Greedy/좌표정렬.js
@@ -0,0 +1,38 @@
+function solution(string) {
+  const arr = string
+    .split('\n')
+    .map((str) => str.split(' ').map((char) => Number(char)));
+
+  arr.shift();
+
+  // bubbleSort
+  // for (let i = 0; i < arr.length - 1; i++) {
+  //   for (let j = 0; j < arr.length - i - 1; j++) {
+  //     const [[leftX, leftY], [rightX, rightY]] = [arr[j], arr[j + 1]];
+
+  //     if ((leftX > rightX) | (leftX === rightX) && leftY > rightY) {
+  //       [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  //     }
+  //   }
+  // }
+
+  arr.sort((a, b) => {
+    if (a[0] < b[0]) return a[0] - b[0];
+    else if (a[0] === b[0]) return a[1] - b[1];
+  });
+  return arr.map((coordinate) => coordinate.join(' ')).join('\n');
+}
+
+console.log(
+  solution(`5
+2 7
+1 3
+1 2
+2 5
+3 6`)
+);
+// 1 2
+// 1 3
+// 2 5
+// 2 7
+// 3 6
