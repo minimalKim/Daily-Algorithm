5af497e4590a54cb8858f2da50fadf0f67997579
kimin3004 <kimin3004@gmail.com>
2022-01-27 11:53:04 +0900

    feat: 순열구하기

---
+++ Recursive+DFS/순열구하기.js
@@ -0,0 +1,37 @@
+function solution(str) {
+  const nums = str.split(/ |\n/).map((char) => Number(char));
+  const [length, capacity] = nums.splice(0, 2);
+  const checks = Array.from({ length }, () => false);
+  const temp = Array.from({ length: capacity });
+  const answer = [];
+
+  function DFS(level, num) {
+    if (level === capacity) {
+      answer.push(temp.join(' '));
+      return;
+    } else {
+      for (let i = 0; i < length; i++) {
+        if (checks[i]) continue;
+        checks[i] = true;
+        temp[level] = nums[i];
+        DFS(level + 1);
+        checks[i] = false;
+      }
+    }
+  }
+
+  DFS(0, 0);
+  return answer.join('\n').concat('\n', answer.length);
+}
+
+console.log(
+  solution(`3 2
+3 6 9`)
+);
+// 3  6
+// 3  9
+// 6  3
+// 6  9
+// 9  3
+// 9  6
+// 6
