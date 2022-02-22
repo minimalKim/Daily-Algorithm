f43cf30ada6eeacec699719fd5f52fdf67721824
kimin3004 <kimin3004@gmail.com>
2022-02-22 11:06:07 +0900

    feat: n과m(4)

---
+++ Recursive+DFS/N과M(4).js
@@ -0,0 +1,25 @@
+function solution(str) {
+  const [maxNumber, capacity] = str.split(' ').map((char) => Number(char));
+  const sequence = [];
+  const answer = [];
+
+  function DFS(level) {
+    if (level === capacity + 1) {
+      answer.push(sequence.join(' '));
+      return;
+    }
+    for (let i = 0; i < maxNumber; i++) {
+      if (i + 1 < sequence[sequence.length - 1]) continue;
+      sequence.push(i + 1);
+      DFS(level + 1);
+      sequence.pop();
+    }
+  }
+
+  DFS(1);
+  return answer.join('\n');
+}
+
+const input = `3 3`;
+
+console.log(solution(input));
