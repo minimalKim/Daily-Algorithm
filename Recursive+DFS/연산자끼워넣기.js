d7b76fa9d67f8dfd0b70eb52cf73984c362d873b
kimin3004 <kimin3004@gmail.com>
2022-02-24 13:18:29 +0900

    feat: 연산자끼워넣기

---
+++ Recursive+DFS/연산자끼워넣기.js
@@ -0,0 +1,56 @@
+//const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+function solution(str) {
+  const [[numberLength], nums, operators] = str
+    .split('\n')
+    .map((str) => str.split(' ').map((char) => Number(char)));
+
+  const results = [];
+  let number = nums[0];
+
+  function DFS(level) {
+    if (level === numberLength) {
+      results.push(number);
+      return;
+    }
+
+    if (operators[0] > 0) {
+      operators[0] -= 1;
+      number += nums[level];
+      DFS(level + 1);
+      operators[0] += 1;
+      number -= nums[level];
+    }
+    if (operators[1] > 0) {
+      operators[1] -= 1;
+      number -= nums[level];
+      DFS(level + 1);
+      operators[1] += 1;
+      number += nums[level];
+    }
+    if (operators[2] > 0) {
+      operators[2] -= 1;
+      number *= nums[level];
+      DFS(level + 1);
+      operators[2] += 1;
+      number /= nums[level];
+    }
+    if (operators[3] > 0) {
+      operators[3] -= 1;
+      let tempNum = number;
+      number = parseInt(number / nums[level]);
+      DFS(level + 1);
+      operators[3] += 1;
+      number = tempNum;
+    }
+  }
+
+  DFS(1);
+  return [Math.max(...results), Math.min(...results)].join('\n');
+}
+
+const input = `6
+1 2 3 4 5 6
+2 1 1 1`;
+
+console.log(solution(input));
