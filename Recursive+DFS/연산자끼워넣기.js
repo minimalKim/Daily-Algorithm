8df1a41856e18c03eba3076d3a3cf64fc33be869
kimin3004 <kimin3004@gmail.com>
2022-02-24 13:19:22 +0900

    refactor: 연산자끼워넣기

--- Recursive+DFS/연산자끼워넣기.js
+++ Recursive+DFS/연산자끼워넣기.js
@@ -49,6 +49,46 @@ function solution(str) {
   return [Math.max(...results), Math.min(...results)].join('\n');
 }
 
+// refactor
+function solution2(str) {
+  const [[numberLength], nums, operators] = str
+    .split('\n')
+    .map((str) => str.split(' ').map((char) => Number(char)));
+
+  const results = [];
+
+  function DFS(level, number) {
+    if (level === numberLength) {
+      results.push(number);
+      return;
+    }
+
+    if (operators[0] > 0) {
+      operators[0] -= 1;
+      DFS(level + 1, number + nums[level]);
+      operators[0] += 1;
+    }
+    if (operators[1] > 0) {
+      operators[1] -= 1;
+      DFS(level + 1, number - nums[level]);
+      operators[1] += 1;
+    }
+    if (operators[2] > 0) {
+      operators[2] -= 1;
+      DFS(level + 1, number * nums[level]);
+      operators[2] += 1;
+    }
+    if (operators[3] > 0) {
+      operators[3] -= 1;
+      DFS(level + 1, parseInt(number / nums[level]));
+      operators[3] += 1;
+    }
+  }
+
+  DFS(1, nums[0]);
+  return [Math.max(...results), Math.min(...results)].join('\n');
+}
+
 const input = `6
 1 2 3 4 5 6
 2 1 1 1`;
