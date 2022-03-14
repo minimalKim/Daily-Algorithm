cfcc306a821ca82e0299c417384952330c5333f2
kimin3004 <kimin3004@gmail.com>
2022-03-14 23:19:27 +0900

    feat: addTwoNumbers

---
+++ LinkedList/AddTwoNumbers.js
@@ -0,0 +1,37 @@
+// 2. Add Two Numbers
+// reference
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+const addTwoNumbers = (l1, l2) => {
+  let sum = 0;
+  let current = new ListNode(0);
+  let result = current;
+
+  while (l1 || l2) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    sum = sum >= 10 ? 1 : 0;
+  }
+
+  if (sum) {
+    current.next = new ListNode(sum);
+  }
+
+  return result.next;
+};
+
+// console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
